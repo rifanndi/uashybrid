42899b04ee5888b9677e54fcc65fa0640e6e861d
rifanndi <rifandiannass@gmail.com>
2024-06-28 17:51:02 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'simpletems',
+  webDir: 'dist'
+};
+
+export default config;
